e44190dcc5565292a4bea282ec3a6b51f59f8cff
Yashas P <130992790+WRenchJr42@users.noreply.github.com>
2023-12-17 17:10:15 +0530

    Create cookie.py

---
+++ cookie.py
@@ -0,0 +1,26 @@
+import requests
+
+
+def get_cookies(url):
+    response = requests.get(url)
+    cookie_dict = {}
+    for key, value in response.cookies.items():
+        cookie_dict[key] = value
+    return cookie_dict
+
+
+def update_cookies_file(url, cookies):
+    filename = "cookies_data.txt"
+
+    with open(filename, 'w+') as file:
+        file.write(f"{url}:\n")
+        for key, value in cookies.items():
+            file.write(f"  {key}: {value}\n")
+        file.write("\n")
+
+
+if __name__ == "__main__":
+    url = input("Enter URL :")
+    cookies = get_cookies(url)
+    update_cookies_file(url, cookies)
+    print(f"Cookies for {url} saved in cookies_data.txt")
